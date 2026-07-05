a913e410a861fd090de78337aa4f2ac663df6723
aginies <aginies@suse.com>
2026-07-05 09:18:42 +0200

    chore: delete redundant older stashfru project backup folder
    - Safely remove the stashfru/ sub-project folder from filesystem and git index.
    
    - Confirmed that the outer root directory contains a much more advanced, up-to-date, and complete version of the codebase.

--- stashfru/prisma/migrations/20260702101004_add_saviez_vous_table/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "SaviezVousFact" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "text" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-
--- CreateIndex
-CREATE INDEX "SaviezVousFact_createdAt_idx" ON "SaviezVousFact"("createdAt");
