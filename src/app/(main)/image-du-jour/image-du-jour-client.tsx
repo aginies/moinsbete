a0b05f49c19deb3b5f1d3e0846d3591a7b636c20
Antoine Ginies <moinsbete@guibo.com>
2026-07-08 20:44:03 +0200

    add fullImage mode to WikipediaImageCard for /image-du-jour page
    When fullImage=true: use object-contain with max-h-[60vh] to show full
    image without cropping. Disable click-to-expand and cursor-pointer.

--- src/app/(main)/image-du-jour/image-du-jour-client.tsx
+++ src/app/(main)/image-du-jour/image-du-jour-client.tsx
@@ -5,7 +5,7 @@ import { WikipediaImageCard } from '@/components/feed/wikipedia-image-card'
 export function ImageDuJourClient() {
   return (
     <div className="w-full">
-      <WikipediaImageCard />
+      <WikipediaImageCard fullImage />
     </div>
   )
 }
