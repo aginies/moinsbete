ab02d54b3d3b385e882ce750073c3c41e6851cce
aginies <aginies@suse.com>
2026-07-10 22:42:37 +0200

    feat(image-wikimedia): enable bookmarking on dedicated page

--- src/app/(main)/image-wikimedia/image-wikimedia-client.tsx
+++ src/app/(main)/image-wikimedia/image-wikimedia-client.tsx
@@ -2,10 +2,14 @@
 
 import { ImageWikimediaCard } from '@/components/feed/image-wikimedia-card'
 
-export function ImageWikimediaClient() {
+interface ImageWikimediaClientProps {
+  userId?: string
+}
+
+export function ImageWikimediaClient({ userId }: ImageWikimediaClientProps) {
   return (
     <div className="w-full">
-      <ImageWikimediaCard fullImage showToggle={false} swipeable={true} />
+      <ImageWikimediaCard fullImage showToggle={false} swipeable={true} userId={userId} />
     </div>
   )
 }
